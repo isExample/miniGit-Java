b17ef47985008f5b62c712ab17811a60dabc815a
isExample <yeji0331@naver.com>
2025-03-11 19:45:06 +0900

    [feat] ignore .miniGit directory in write-tree command
    - isIgnored() method manage ignored paths

--- src/main/java/data/MiniGitCore.java
+++ src/main/java/data/MiniGitCore.java
@@ -15,12 +15,18 @@ public class MiniGitCore {
             Files.walkFileTree(rootPath, new SimpleFileVisitor<Path>() {
                 @Override
                 public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+                    if (isIgnored(file)) {
+                        return FileVisitResult.CONTINUE;
+                    }
                     System.out.println(file.toAbsolutePath());
                     return FileVisitResult.CONTINUE;
                 }
 
                 @Override
                 public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
+                    if (isIgnored(dir)) {
+                        return FileVisitResult.SKIP_SUBTREE; // 하위 디렉터리 탐색 skip
+                    }
                     System.out.println(dir.toAbsolutePath());
                     return FileVisitResult.CONTINUE;
                 }
@@ -29,5 +35,9 @@ public class MiniGitCore {
             System.err.println("Error: Could not traverse directory.");
             e.printStackTrace();
         }
+    }
+
+    private static boolean isIgnored(Path path) {
+        return path.toAbsolutePath().toString().contains(".miniGit");
     }
 }
