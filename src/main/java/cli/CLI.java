30a965027d4cb2188232b9509ff774c4a7197bae
isExample <yeji0331@naver.com>
2025-03-11 12:04:13 +0900

    [feat] add picocli to basic structure

--- src/main/java/cli/CLI.java
+++ src/main/java/cli/CLI.java
@@ -1,5 +1,8 @@
 package cli;
 
+import picocli.CommandLine;
+
+@CommandLine.Command(name = "miniGit", mixinStandardHelpOptions = true, subcommands = {InitCommand.class})
 public class CLI {
     public static void run() {
         System.out.println("Hello world!");
--- src/main/java/cli/Main.java
+++ src/main/java/cli/Main.java
@@ -1,7 +1,10 @@
 package cli;
 
+import picocli.CommandLine;
+
 public class Main {
     public static void main(String[] args) {
-        CLI.run();
+        int exitCode = new CommandLine(new CLI()).execute(args);
+        System.exit(exitCode);
     }
 }
