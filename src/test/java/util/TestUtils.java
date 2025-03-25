f49050295f42a3ff24f5545c746ac93002de8e66
isExample <yeji0331@naver.com>
2025-03-25 22:25:34 +0900

    [test] add file I/O utilitys for testing

---
+++ src/test/java/util/TestUtils.java
@@ -0,0 +1,26 @@
+package util;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class TestUtils {
+    public static void writeFile(Path dir, String name, String content) throws IOException {
+        Path file = dir.resolve(name);
+        Files.createDirectories(file.getParent());
+        Files.writeString(file, content);
+    }
+
+    public static String readFile(Path dir, String name) throws IOException {
+        Path file = dir.resolve(name);
+        return Files.readString(file);
+    }
+
+    public static boolean fileExists(Path dir, String name) {
+        return Files.exists(dir.resolve(name));
+    }
+
+    public static void deleteFile(Path dir, String name) throws IOException {
+        Files.deleteIfExists(dir.resolve(name));
+    }
+}
